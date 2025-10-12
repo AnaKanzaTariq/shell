c772095ce5c88ac30594f1ef6a607565607e6123
Kanza <kanza.tariq@mail.utoronto.ca>
2025-10-12 17:50:43 -0400

    updated assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -34,7 +34,7 @@ cd data
 mkdir raw
 cd ~/Desktop/DSI_assignment1/shell/02_activities/assignments
 unzip -q rawdata.zip
-mv ./rawdata ./newproject/data/raw
+mv ./rawdata/* ./newproject/data/raw/
 # 3. List the contents of the ./data/raw directory
 ls ./newproject/data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
@@ -43,14 +43,14 @@ mkdir processed
 cd processed
 mkdir server_logs user_logs event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-cd ~/Desktop/DSI_assignment1/shell/02_activities/assignments/newproject/data/raw/rawdata
+cd ~/Desktop/DSI_assignment1/shell/02_activities/assignments/newproject/data/raw
 cp *server*.log ~/Desktop/DSI_assignment1/shell/02_activities/assignments/newproject/data/processed/server_logs
 # 6. Repeat the above step for user logs and event logs
-cd ~/Desktop/DSI_assignment1/shell/02_activities/assignments/newproject/data/raw/rawdata
+cd ~/Desktop/DSI_assignment1/shell/02_activities/assignments/newproject/data/raw
 cp *user*.log ~/Desktop/DSI_assignment1/shell/02_activities/assignments/newproject/data/processed/user_logs
 cp *event*.log ~/Desktop/DSI_assignment1/shell/02_activities/assignments/newproject/data/processed/event_logs
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-cd ~/Desktop/DSI_assignment1/shell/02_activities/assignments/newproject/data/raw/rawdata
+cd ~/Desktop/DSI_assignment1/shell/02_activities/assignments/newproject/data/raw
 rm *ipaddr*
 cd ~/Desktop/DSI_assignment1/shell/02_activities/assignments/newproject/data/processed/user_logs
 rm *ipaddr*
