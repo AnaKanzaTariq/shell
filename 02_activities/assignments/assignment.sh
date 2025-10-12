fd1a9e4d4298d20860242c64807c20703dd0b73e
Kanza <kanza.tariq@mail.utoronto.ca>
2025-10-11 23:08:42 -0400

    Updated assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -28,20 +28,36 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
-
+mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
-
+cd data
+mkdir raw
+cd ~/Desktop/DSI_assignment1/shell/02_activities/assignments
+unzip -q rawdata.zip
+mv ./rawdata ./newproject/data/raw
 # 3. List the contents of the ./data/raw directory
-
+ls ./newproject/data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
-
+cd ./newproject/data
+mkdir processed
+cd processed
+mkdir server_logs user_logs event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-
+cd ~/Desktop/DSI_assignment1/shell/02_activities/assignments/newproject/data/raw/rawdata
+cp *server*.log ~/Desktop/DSI_assignment1/shell/02_activities/assignments/newproject/data/processed/server_logs
 # 6. Repeat the above step for user logs and event logs
-
+cd ~/Desktop/DSI_assignment1/shell/02_activities/assignments/newproject/data/raw/rawdata
+cp *user*.log ~/Desktop/DSI_assignment1/shell/02_activities/assignments/newproject/data/processed/user_logs
+cp *event*.log ~/Desktop/DSI_assignment1/shell/02_activities/assignments/newproject/data/processed/event_logs
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-
+cd ~/Desktop/DSI_assignment1/shell/02_activities/assignments/newproject/data/raw/rawdata
+rm *ipaddr*
+cd ~/Desktop/DSI_assignment1/shell/02_activities/assignments/newproject/data/processed/user_logs
+rm *ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
+cd ~/Desktop/DSI_assignment1/shell/02_activities/assignments/newproject/data
+touch inventory.txt
+find ~/Desktop/DSI_assignment1/shell/02_activities/assignments/newproject/data/processed -type f > ~/Desktop/DSI_assignment1/shell/02_activities/assignments/newproject/data/inventory.txt
 
 
 ###########################################
